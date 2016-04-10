4e440989f95a0453eb9c0656b7f7c7d76d727057
David Krska <david_krska@volny.cz>
2016-04-10 09:40:51 +0200

    Add function to create filter

--- nl/nl_link.c
+++ nl/nl_link.c
@@ -1,7 +1,5 @@
 #include "nl_link.h"
 
-#include <linux/if.h>
-
 #include <netlink/netlink.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/veth.h>
@@ -10,6 +8,8 @@
 #include <netlink/route/class.h>
 #include <netlink/route/classifier.h>
 #include <netlink/route/addr.h>
+
+#include <linux/if.h>
 
 #include <unistd.h>
 #include <stdio.h>
@@ -302,7 +302,7 @@ int nl_link_ingress_add_qdisc(const char* if_name)
 
     rtnl_tc_set_link(TC_CAST(qdisc), link);
     rtnl_tc_set_parent(TC_CAST(qdisc), TC_H_INGRESS);
-    rtnl_tc_set_handle(TC_CAST(qdisc), TC_HANDLE(0xffff, 0));
+    rtnl_tc_set_handle(TC_CAST(qdisc), TC_HANDLE(0xfffa, 0));
     err = rtnl_tc_set_kind(TC_CAST(qdisc), "ingress");
     if (err < 0){
         nl_perror(err, "Can not allocate ingress");
@@ -326,4 +326,134 @@ int nl_link_ingress_add_qdisc(const char* if_name)
 	nl_socket_free(sk);
 
 	return NLE_SUCCESS;
+}
+
+// Create filter hash table
+int u32_add_ht(struct nl_sock *sock, struct rtnl_link *rtnlLink,
+		uint32_t prio, uint32_t htid, uint32_t divisor){
+
+	int err;
+	struct rtnl_cls *cls;
+
+	cls=rtnl_cls_alloc();
+	if (!(cls)) {
+		printf("Can not allocate classifier\n");
+		nl_socket_free(sock);
+		exit(1);
+	}
+
+	rtnl_tc_set_link(TC_CAST(cls), rtnlLink);
+
+	if ((err = rtnl_tc_set_kind(TC_CAST(cls), "u32"))) {
+		printf("Can not set classifier as u32\n");
+		return 1;
+	}
+
+	rtnl_cls_set_prio(cls, prio);
+	rtnl_cls_set_protocol(cls, ETH_P_IP);
+	rtnl_tc_set_parent(TC_CAST(cls), TC_HANDLE(0xffff, 0));
+
+	// rtnl_tc_set_handle((struct rtnl_tc *) cls, htid);
+	rtnl_u32_set_handle(cls, htid, 0x0, 0x0);
+	rtnl_u32_set_divisor(cls, divisor);
+
+	// if ((err = rtnl_cls_add(sock, cls, NLM_F_CREATE | NLM_F_EXCL))) {
+	if ((err = rtnl_cls_add(sock, cls, NLM_F_CREATE))) {
+		printf("Can not add classifier: %s\n", nl_geterror(err));
+		return -1;
+	}
+	rtnl_cls_put(cls);
+	return 0;
+}
+
+// Copy pasted from libnl
+int get_u32(__u32 *val, const char *arg, int base)
+{
+	unsigned long res;
+	char *ptr;
+
+	if (!arg || !*arg)
+		return -1;
+	res = strtoul(arg, &ptr, base);
+	if (!ptr || ptr == arg || *ptr || res > 0xFFFFFFFFUL)
+		return -1;
+	*val = res;
+	return 0;
+}
+
+// Copy pasted from libnl
+int get_u32_handle(__u32 *handle, const char *str)
+{
+	__u32 htid=0, hash=0, nodeid=0;
+	char *tmp = strchr(str, ':');
+        
+	if (tmp == NULL) {
+		if (memcmp("0x", str, 2) == 0)
+			return get_u32(handle, str, 16);
+		return -1;
+	}
+	htid = strtoul(str, &tmp, 16);
+	if (tmp == str && *str != ':' && *str != 0)
+		return -1;
+	if (htid>=0x1000)
+		return -1;
+	if (*tmp) {
+		str = tmp+1;
+		hash = strtoul(str, &tmp, 16);
+		if (tmp == str && *str != ':' && *str != 0)
+			return -1;
+		if (hash>=0x100)
+			return -1;
+		if (*tmp) {
+			str = tmp+1;
+			nodeid = strtoul(str, &tmp, 16);
+			if (tmp == str && *str != 0)
+				return -1;
+			if (nodeid>=0x1000)
+				return -1;
+		}
+	}
+	*handle = (htid<<20)|(hash<<12)|nodeid;
+	return 0;
+}
+
+/*
+ * Same as: 'tc filter add dev <rtnlLink> parent ffff: protocol all prio <prio> \
+ *           u32 match u32 <keyval> <keymask> at <keyoff> <act>'
+ *    (e.g. act = action mirred egress redirect dev qwer)
+ */
+// TODO: find out what 'keyoffmask' is good for?
+int u32_add_filter_with_hashmask(struct nl_sock *sock, struct rtnl_link *rtnlLink, uint32_t prio, 
+		uint32_t keyval, uint32_t keymask, int keyoff, int keyoffmask, struct rtnl_act *act){
+
+	struct rtnl_cls *cls;
+	int err;
+
+	cls=rtnl_cls_alloc();
+	if (!(cls)) {
+		printf("Can not allocate classifier\n");
+		nl_socket_free(sock);
+		exit(1);
+	}
+
+	rtnl_tc_set_link(TC_CAST(cls), rtnlLink);
+
+	if ((err = rtnl_tc_set_kind(TC_CAST(cls), "u32"))) {
+		printf("Can not set classifier as u32\n");
+		return 1;
+	}
+
+	rtnl_cls_set_prio(cls, prio);
+	rtnl_cls_set_protocol(cls, ETH_P_ALL);
+	rtnl_tc_set_parent(TC_CAST(cls), TC_HANDLE(0xffff, 0));
+
+	rtnl_u32_add_key_uint32(cls, keyval, keymask, keyoff, keyoffmask);
+
+	rtnl_u32_add_action(cls, act);
+
+	if ((err = rtnl_cls_add(sock, cls, NLM_F_CREATE))) {
+		printf("Can not add classifier: %s\n", nl_geterror(err));
+		return -1;
+	}
+	rtnl_cls_put(cls);
 }
--- nl/nl_link.h
+++ nl/nl_link.h
@@ -8,6 +8,9 @@
 #include <netlink/route/qdisc.h>
 #include <netlink/route/class.h>
 #include <netlink/route/classifier.h>
+#include <netlink/route/cls/u32.h>
+
+#include <linux/if_ether.h>
 
 #include <unistd.h>
 #include <stdio.h>
@@ -22,11 +25,21 @@ int nl_link_assign_addr(const char* if_name, const char* mac);
 
 int nl_link_ingress_add_qdisc(const char* if_name);
 
-int nl_link_add_qdisc(const char* if_name, uint32_t parent, uint32_t handle);
+// int nl_link_add_qdisc(const char* if_name, uint32_t parent, uint32_t handle);
 
-//int nl_link_del_qdisc(const char* if_name, );
+// int nl_link_del_qdisc(const char* if_name, );
 
 // Utils
 struct rtnl_link *rtnl_link_dummy_alloc(void);
+
+int u32_add_ht(struct nl_sock *sock, struct rtnl_link *rtnlLink,
+		uint32_t prio, uint32_t htid, uint32_t divisor);
+
+int get_u32(__u32 *val, const char *arg, int base);
+
+int get_u32_handle(__u32 *handle, const char *str);
+int u32_add_filter_with_hashmask(struct nl_sock *sock, struct rtnl_link *rtnlLink, uint32_t prio, 
+	    uint32_t keyval, uint32_t keymask, int keyoff, int keyoffmask, struct rtnl_act *act);
+
 
 #endif
