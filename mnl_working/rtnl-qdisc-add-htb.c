8240a45016ceccf2dad48b447ef9adb44947dc2c
David Krska <david_krska@volny.cz>
2016-05-17 22:53:00 +0200

    Add test for htb qdisc creation using libmnl.

--- mnl_working/rtnl-qdisc-add-htb.c
+++ mnl_working/rtnl-qdisc-add-htb.c
@@ -6,46 +6,55 @@
 #include <errno.h>
 
 #include <libmnl/libmnl.h>
-#include <linux/if.h>
-#include <linux/if_link.h>
 #include <linux/rtnetlink.h>
+#include <linux/pkt_sched.h>
 
 /**
  * Using iproute:
- * # ip link add dev <ifname> type dummy
+ * # ip link add dev <if_name> type dummy
+ *
+ * note: for simplicity this program uses if_index insted of if_name
+ * TODO: use if_name instead of if_index
  */
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2) {
-		printf("Usage: %s <ifname>\n", argv[0]);
+		printf("Usage: %s <if_index>\n", argv[0]);
 		return -1;
 	}
 
-	const char *if_name = argv[1];
-	const char *if_type = "dummy";
+	size_t if_index = atoi(argv[1]);
+	const char *tc_kind = "htb";
 
 	char buf[MNL_SOCKET_BUFFER_SIZE];
 	int ret;
 
 	// Init netlink header
 	struct nlmsghdr *nlh = mnl_nlmsg_put_header(buf);
-	nlh->nlmsg_type	= RTM_NEWLINK;
+	nlh->nlmsg_type	= RTM_NEWQDISC;
 	nlh->nlmsg_flags = NLM_F_CREATE | NLM_F_REQUEST | NLM_F_ACK;
 	nlh->nlmsg_seq = time(NULL);
 
-	// Init link extra header
-	struct ifinfomsg *ifm = mnl_nlmsg_put_extra_header(nlh, sizeof(*ifm));
-	ifm->ifi_family = AF_UNSPEC;
-	ifm->ifi_change = 0;
-	ifm->ifi_flags = 0;
+	// Init extra header
+	struct tcmsg *tcm = mnl_nlmsg_put_extra_header(nlh, sizeof(*tcm));
+	// tcm->tcm_family = AF_INET;
+	tcm->tcm_family = AF_UNSPEC;
+	tcm->tcm_ifindex = if_index;
+	// TODO: report bug - macros for handle manipulation are useless
+	tcm->tcm_handle = 0x00010000U;
+	tcm->tcm_parent = TC_H_ROOT;
 
 	// Add attributes
-	mnl_attr_put_str(nlh, IFLA_IFNAME, if_name);
-	struct nlattr* nested_start = 
-		mnl_attr_nest_start_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_LINKINFO);
-	mnl_attr_put_str(nlh, IFLA_INFO_KIND, if_type);
-	mnl_attr_nest_end(nlh, nested_start);
+	mnl_attr_put_str(nlh, TCA_KIND, tc_kind);
+	struct nlattr* nested_attr = mnl_attr_nest_start(nlh, TCA_OPTIONS);
+
+	struct tc_htb_glob opts = {
+		.version = TC_HTB_PROTOVER,
+		.rate2quantum = 10,
+	};
+	mnl_attr_put(nlh, TCA_HTB_INIT, sizeof(opts), &opts);
+	mnl_attr_nest_end(nlh, nested_attr);
 
 	// Open and bind socket connection
 	struct mnl_socket *nl = mnl_socket_open(NETLINK_ROUTE);
@@ -64,6 +73,7 @@ int main(int argc, char *argv[])
 	printf("Sending following message:\n");
 	mnl_nlmsg_fprintf(stdout, nlh, nlh->nlmsg_len,
 			  sizeof(struct ifinfomsg));
+	// BUG: prints only first 4 words of extra header, tcmsg has 5 words
 
 	ret = mnl_socket_sendto(nl, nlh, nlh->nlmsg_len);
 	if (ret == -1) {
@@ -93,6 +103,6 @@ int main(int argc, char *argv[])
 	}
 
 	mnl_socket_close(nl);
-
 	return 0;
 }
+
