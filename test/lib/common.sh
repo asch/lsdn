58b3f42bfec0338170f58cc9476c27c1e4aba4c8
Roman Kapl <code@rkapl.cz>
2016-07-28 00:41:08 +0200

    Test reorganization.
    Parts of tests can be run, more things are in the harness.
    Added qemu boilerplate code.

--- test/lib/common.sh
+++ test/lib/common.sh
@@ -21,3 +21,27 @@ function ping_from() {
 	dst=$2
 	IN_NS $src ping -c 1 -w 1 $IPV4_PREFIX$dst
 }
+
+function test_error() {
+	echo "^^^ TEST FAILED ^^^"
+	$cleanup_handler
+	exit 1
+}
+
+function fail() {	
+	if trace_command "$@"; then
+		test_error
+	fi
+}
+
+function test() {
+	if ! trace_command "$@"; then
+		test_error
+	fi
+}
+
+function trace_command {
+	echo "> $@"
+	"$@"
+	return $?
+}
--- test/test_switches.sh
+++ test/test_switches.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
+function verify_env
+{
+	fail ping_from 1 4
+}
 
-source common.sh
-
-if ping_from 1 4; then
-	echo "ping works before switches set up, env is broken"
-	exit 1
-fi
-
-./test_switches
-
-ping_from 1 4
+function verify
+{
+	test ping_from 1 4
+}
