9b1c90fbdfa8387fa614d09e94f00d8b17c73394
David Čepelík <cepelik@gymlit.cz>
2016-08-02 11:57:41 +0200

    Use netmodel/include/util.h debug macros within lsdnctl.

--- lsdnctl/common.h
+++ lsdnctl/common.h
@@ -1,25 +1,12 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include "../netmodel/include/util.h"
 #include <stddef.h>
 
 #ifndef	DEBUG
 #define	DEBUG	1
 #endif
-
-#define	DEBUG_PRINTF(fmt, ...) do { \
-	if (DEBUG) fprintf(stderr, "*** DEBUG OUTPUT ***\t" fmt " at %s:%d\n", \
-		__VA_ARGS__, __FILE__, __LINE__); \
-} while (0);
-
-#define	DEBUG_MSG(msg)			DEBUG_PRINTF("%s", msg);
-#define	DEBUG_EXPR(formatter, expr)	DEBUG_PRINTF(#expr " = " formatter, (expr));
-
-#define NOT_IMPLEMENTED	do {\
-	DEBUG_PRINTF("%s: not implemented", __func__);\
-	exit(EXIT_FAILURE);\
-} while (0);
-
 
 void *malloc_safe(size_t size);
 void *realloc_safe(void *ptr, size_t size);
