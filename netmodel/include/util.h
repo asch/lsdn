ce1fbe10a8114d7a7f78c9e7e5954ab62a505621
David Čepelík <cepelik@gymlit.cz>
2016-08-02 11:53:08 +0200

    Add useful debug macros to netmodel/include/util.h.
    This allows for code reuse with lsdnctl.

--- netmodel/include/util.h
+++ netmodel/include/util.h
@@ -3,4 +3,17 @@
 
 #define UNUSED(x) ((void)(x))
 
+#define	DEBUG_PRINTF(fmt, ...) do { \
+	if (DEBUG) fprintf(stderr, "*** DEBUG OUTPUT ***\t" fmt " at %s:%d\n", \
+		__VA_ARGS__, __FILE__, __LINE__); \
+} while (0);
+
+#define	DEBUG_MSG(msg)			DEBUG_PRINTF("%s", msg);
+#define	DEBUG_EXPR(formatter, expr)	DEBUG_PRINTF(#expr " = " formatter, (expr));
+
+#define	NOT_IMPLEMENTED	do {\
+	DEBUG_PRINTF("%s: not implemented", __func__); \
+	exit(EXIT_FAILURE); \
+} while (0);
+
 #endif
