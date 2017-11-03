f4023a02fdd161d6f3af8fa48232ed3452b5a58d
Vojtech Aschenbrenner <v@asch.cz>
2017-11-03 14:28:47 +0100

    Use package cache when creating rootfs

--- test/create_rootfs.sh
+++ test/create_rootfs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+
 rm -rf rootfs
 mkdir -p rootfs/var/lib/pacman
-fakeroot -- pacman --noconfirm -r rootfs -Syy base cmake valgrind gdb tcl dhcp tcpdump
+mkdir -p pacman-cache
+fakeroot -- pacman --cachedir $(pwd)/pacman-cache/ --noconfirm -r rootfs -Syy base cmake valgrind gdb tcl dhcp tcpdump
 chmod -s rootfs/bin/mount
 cp qemu/lsdn-guest-init rootfs/
