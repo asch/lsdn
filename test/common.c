80016d5ba112f11557d506250b5d945c4b0c3994
Adam Vyskovsky <adamvyskovsky@gmail.com>
2017-12-16 23:22:57 +0100

    Use default vxlan and geneve ports in tests.

--- test/common.c
+++ test/common.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+static uint16_t vxlan_port = 4789;
+static uint16_t geneve_port = 6081;
+
 struct lsdn_settings *settings_from_env(struct lsdn_context *ctx) {
 	const char *nettype = getenv("LSCTL_NETTYPE");
 	if (!nettype) {
@@ -10,15 +13,15 @@ struct lsdn_settings *settings_from_env(struct lsdn_context *ctx) {
 	} else if (!strcmp(nettype, "vlan")) {
 		return lsdn_settings_new_vlan(ctx);
 	} else if (!strcmp(nettype, "vxlan/e2e")) {
-		return lsdn_settings_new_vxlan_e2e(ctx, 0);
+		return lsdn_settings_new_vxlan_e2e(ctx, vxlan_port);
 	} else if (!strcmp(nettype, "vxlan/static")) {
-		return lsdn_settings_new_vxlan_static(ctx, 0);
+		return lsdn_settings_new_vxlan_static(ctx, vxlan_port);
 	} else if (!strcmp(nettype, "vxlan/mcast")) {
-		return lsdn_settings_new_vxlan_mcast(ctx, LSDN_MK_IPV4(239,239,239,239), 0);
+		return lsdn_settings_new_vxlan_mcast(ctx, LSDN_MK_IPV4(239,239,239,239), vxlan_port);
 	} else if (!strcmp(nettype, "direct")) {
 		return lsdn_settings_new_direct(ctx);
 	} else if (!strcmp(nettype, "geneve")) {
-		return lsdn_settings_new_geneve(ctx, 0);
+		return lsdn_settings_new_geneve(ctx, geneve_port);
 	} else {
 		fprintf(stderr, "Unknown nettype: %s\n", nettype);
 		abort();
