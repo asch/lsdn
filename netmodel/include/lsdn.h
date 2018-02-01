a9e10c75ff5000d60d0d33b1fb9d30b1f5a5eb68
matejcik <ja@matejcik.cz>
2018-02-01 15:32:52 +0100

    doxygen module grouping

--- netmodel/include/lsdn.h
+++ netmodel/include/lsdn.h
@@ -30,9 +30,48 @@
 @see @ref attributes. */ \
 	const type *lsdn_##obj##_get_##attr(struct lsdn_##obj *obj)
 
-struct lsdn_virt;
+
+/* XXX the following struct documentation snippets are NOT USED.
+ * I'm not sure why. Instead, descriptions in private/lsdn.h are used,
+ * and these structs are not even listed in the public header documentation.
+ * What do.? */
+
+/** Context.
+ * This is documented elsewhere. */
+struct lsdn_context;
+
+/** Virtual network representation.
+ * Nodes defined by `lsdn_virt` connect to a `lsdn_net` through physical host connections `lsdn_phys`.
+ * Can be implemented using common tunneling techniques, like vlan or vxlan or no tunneling.
+ *
+ * Networks are defined by two main characteristics:
+ *  - the tunnel used to overlay the network over physical topology (transparent to end users)
+ *  - the switching methods used (visible to end users) */
 struct lsdn_net;
+
+/** Node in the virtual network.
+ * A virtual machine (typically -- it may be any linux interface).
+ *
+ * Virtual machines participate in virtual networks (through phys_attachments on the host machine
+ * connection). They can be migrated between the physical machines by connecting them through
+ * different `lsdn_phys`. */
+struct lsdn_virt;
+
+/** Physical host connection representation.
+ * Represents a kernel interface for a node, e.g., eth0 on lsdn1.
+ * Physical interfaces are used to connect to virtual networks. This connection is called
+ * `lsdn_phys_attachement`. */
 struct lsdn_phys;
+
+/** Physical interface attachment.
+ * A point of connection to a virtual network through a physical interface.
+ * Only a single attachment may exist for a pair of `lsdn_phys` and `lsdn_net`. */
+struct lsdn_phys_attachment;
+
+/** Configuration structure for `lsdn_net`.
+ * Multiple networks can share the same settings (e.g. vxlan with static routing on port 1234)
+ * and only differ by their identifier (vlan id, vni ...). */
+struct lsdn_settings;
 
 /** Virt rule direction. */
 enum lsdn_direction {
@@ -42,6 +81,7 @@ enum lsdn_direction {
 	LSDN_OUT
 };
 
+/** Signature for out-of-memory callback. */
 typedef void (*lsdn_nomem_cb)(void *user);
 
 /** User callback hooks.
@@ -63,17 +103,8 @@ struct lsdn_user_hooks {
 	void *lsdn_shutdown_hook_user;
 };
 
-/**
- * Top-level context encompassing all network topology. This includes virtual networks
- * (lsdn_network) and physical host connections (lsdn_phys). Only one context will typically exist
- * in a given program.
- *
- * The same structures (lsdn_phys, lsdn_virt) are used to describe both remote objects
- * and objects running on other machines. This allows the orchestrator to make the same API calls
- * on all physical machines to construct the network topology. The only difference between the
- * API calls on the physical machines will be the lsdn_phys_claim_local calls. */
-struct lsdn_context;
-
+/** @defgroup context Context management
+ * @{ */
 struct lsdn_context *lsdn_context_new(const char* name);
 void lsdn_context_set_nomem_callback(struct lsdn_context *ctx, lsdn_nomem_cb cb, void *user);
 void lsdn_context_abort_on_nomem(struct lsdn_context *ctx);
@@ -81,8 +112,12 @@ void lsdn_context_free(struct lsdn_context *ctx);
 /* Will automatically delete all child objects */
 void lsdn_context_cleanup(struct lsdn_context *ctx, lsdn_problem_cb cb, void *user);
 
+lsdn_err_t lsdn_validate(struct lsdn_context *ctx, lsdn_problem_cb cb, void *user);
+lsdn_err_t lsdn_commit(struct lsdn_context *ctx, lsdn_problem_cb cb, void *user);
+/** @} */
+
 /** Type of network encapsulation. */
-enum lsdn_nettype{
+enum lsdn_nettype {
 	/** VXLAN encapsulation. */
 	LSDN_NET_VXLAN,
 	/** VLAN encapsulation. */
@@ -94,7 +129,7 @@ enum lsdn_nettype{
 };
 
 /** Switch type for the virtual network. */
-enum lsdn_switch{
+enum lsdn_switch {
 	/** Learning switch with single tunnel shared from the phys.
 	 * The network is essentially autoconfiguring in this mode. */
 	LSDN_LEARNING,
@@ -114,11 +149,8 @@ enum lsdn_switch{
 	 * interface to be switched by some sort of learning switch. May be added if it appears. */
 };
 
-/** Configuration structure for `lsdn_net`.
- * Multiple networks can share the same settings (e.g. vxlan with static routing on port 1234)
- * and only differ by their identifier (vlan id, vni ...). */
-struct lsdn_settings;
-
+/** @defgroup settings Network settings
+ * @{ */
 struct lsdn_settings *lsdn_settings_new_direct(struct lsdn_context *ctx);
 struct lsdn_settings *lsdn_settings_new_vlan(struct lsdn_context *ctx);
 struct lsdn_settings *lsdn_settings_new_vxlan_mcast(struct lsdn_context *ctx, lsdn_ip_t mcast_ip, uint16_t port);
@@ -130,30 +162,20 @@ void lsdn_settings_register_user_hooks(struct lsdn_settings *settings, struct ls
 lsdn_err_t lsdn_settings_set_name(struct lsdn_settings *s, const char *name);
 const char* lsdn_settings_get_name(struct lsdn_settings *s);
 struct lsdn_settings *lsdn_settings_by_name(struct lsdn_context *ctx, const char *name);
+/** @} */
 
-
-/** Virtual network representation.
- * Nodes defined by `lsdn_virt` connect to a `lsdn_net` through physical host connections `lsdn_phys`.
- * Can be implemented using common tunneling techniques, like vlan or vxlan or no tunneling.
- *
- * Networks are defined by two main characteristics:
- *  - the tunnel used to overlay the network over physical topology (transparent to end users)
- *  - the switching methods used (visible to end users) */
-struct lsdn_net;
-
+/** @defgroup network Virtual networks 
+ * @{ */
 struct lsdn_net *lsdn_net_new(struct lsdn_settings *settings, uint32_t vnet_id);
 lsdn_err_t lsdn_net_set_name(struct lsdn_net *net, const char *name);
 const char* lsdn_net_get_name(struct lsdn_net *net);
 struct lsdn_net* lsdn_net_by_name(struct lsdn_context *ctx, const char *name);
 /* Will automatically delete all child objects */
 void lsdn_net_free(struct lsdn_net *net);
+/** @}*/
 
-/** Physical host connection representation.
- * Represents a kernel interface for a node, e.g., eth0 on lsdn1.
- * Physical interfaces are used to connect to virtual networks. This connection is called
- * `lsdn_phys_attachement`. */
-struct lsdn_phys;
-
+/** @defgroup phys Phys management
+ * @{ */
 struct lsdn_phys *lsdn_phys_new(struct lsdn_context *ctx);
 lsdn_err_t lsdn_phys_set_name(struct lsdn_phys *phys, const char *name);
 const char* lsdn_phys_get_name(struct lsdn_phys *phys);
@@ -167,21 +189,11 @@ lsdn_err_t lsdn_phys_unclaim_local(struct lsdn_phys *phys);
 
 LSDN_DECLARE_ATTR(IP address, phys, ip, lsdn_ip_t);
 LSDN_DECLARE_ATTR(interface, phys, iface, const char*);
-
-/** Physical interface attachment.
- * A point of connection to a virtual network through a physical interface.
- * Only a single attachment may exist for a pair of `lsdn_phys` and `lsdn_net`. */
-struct lsdn_phys_attachment;
+/** @} */
 
 
-/** Node in the virtual network.
- * A virtual machine (typically -- it may be any linux interface).
- *
- * Virtual machines participate in virtual networks (through phys_attachments on the host machine
- * connection). They can be migrated between the physical machines by connecting them through
- * different `lsdn_phys`. */
-struct lsdn_virt;
-
+/** @defgroup virt Virt management
+ * @{ */
 struct lsdn_virt *lsdn_virt_new(struct lsdn_net *net);
 void lsdn_virt_free(struct lsdn_virt* vsirt);
 struct lsdn_net *lsdn_virt_get_net(struct lsdn_virt *virt);
@@ -196,9 +208,9 @@ void lsdn_virt_disconnect(struct lsdn_virt *virt);
  * committed stated). The phys interface must already exist. */
 lsdn_err_t lsdn_virt_get_recommended_mtu(struct lsdn_virt *virt, unsigned int *mtu);
 
-/** A bandwith limit for virt's interface (for one direction).
- * @see lsdn_set_qos_rate
- */
+/** Bandwith limit for virt's interface (for one direction).
+ * @ingroup virt
+ * @see lsdn_set_qos_rate */
 typedef struct {
 	/* in bytes per second */
 	float avg_rate;
@@ -211,8 +223,7 @@ typedef struct {
 LSDN_DECLARE_ATTR(MAC address, virt, mac, lsdn_mac_t);
 LSDN_DECLARE_ATTR(inbound bandwith limit, virt, rate_in, lsdn_qos_rate_t);
 LSDN_DECLARE_ATTR(outbound bandwith limit, virt, rate_out, lsdn_qos_rate_t);
-lsdn_err_t lsdn_validate(struct lsdn_context *ctx, lsdn_problem_cb cb, void *user);
-lsdn_err_t lsdn_commit(struct lsdn_context *ctx, lsdn_problem_cb cb, void *user);
+/** @} */
 
 const char *lsdn_mk_name(struct lsdn_context *ctx, const char *type);
 
