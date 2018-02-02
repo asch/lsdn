f181decb10b2c29030a525bd34345830aed21cbb
Roman Kapl <code@rkapl.cz>
2018-02-02 19:07:24 +0100

    lsdn->LSDN, remove texinfo and man

--- docs/conf.py
+++ docs/conf.py
@@ -133,30 +133,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'lsdn.tex', 'lsdn Documentation',
+    (master_doc, 'lsdn.tex', 'LSDN Documentation',
      'LSDN Collective', 'manual'),
-]
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'lsdn', 'lsdn Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'lsdn', 'lsdn Documentation',
-     author, 'lsdn', 'One line description of project.',
-     'Miscellaneous'),
 ]
 
 def setup(app):
